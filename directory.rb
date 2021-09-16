800803e8c1712a77c337a242c0abe6b21b65b326
Conor <cwdbutler@gmail.com>
2021-09-16 09:41:57 +0000

    User inputs students, printing by letter, printing with number

--- directory.rb
+++ directory.rb
@@ -1,17 +1,16 @@
-# putting students into an array of hashes
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice" # an empty input will break the loop below
+  students = [] # stores list of students
+  name = gets.chomp  # discards return (newline)
+  while !name.empty? do # if the user inputs something
+    students << {name: name, cohort: :november} # << means .push/.append
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  # return the array of students (implicitly)
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -20,8 +19,14 @@ end
 
 # printing each student
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "Print student names beginning with which letter? Type enter to print all students"
+  letter = gets.chomp
+  index = 0
+  until index == students.length - 1 do
+    if (students[index][:name].downcase[0] == (letter.downcase) || letter.empty?) && students[index][:name].length < 12
+      puts "#{index + 1}. #{students[index][:name]} (#{students[index][:cohort]} cohort)"
+    end
+    index += 1
   end
 end
 
@@ -31,6 +36,7 @@ def print_footer(students)
 end
 
 # calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
