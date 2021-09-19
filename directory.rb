336f44f87feb06761f0c6fa597617b37e0e12993
Conor <cwdbutler@gmail.com>
2021-09-19 09:48:52 +0000

    Loading students

--- directory.rb
+++ directory.rb
@@ -46,6 +46,15 @@ def save_students
   file.close
 end
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort, country, height = line.chomp.split(',')
+    @students << {name: name, cohort: cohort, country: country, height: height}
+  end
+  file.close
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------------------------------"
@@ -91,6 +100,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"  
 end
 
@@ -107,6 +117,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
