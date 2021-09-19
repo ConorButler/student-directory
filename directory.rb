c50a5c94e582c4306a0b46cbca4d4b5551e0cbd6
Conor <cwdbutler@gmail.com>
2021-09-19 07:42:36 +0000

    Printing by cohort, standardising input

--- directory.rb
+++ directory.rb
@@ -1,5 +1,4 @@
 def input_students
-  puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
   # an empty input will break the loop below
   students = [] # stores list of students
   i = 0
@@ -10,16 +9,28 @@ def input_students
     if name == "stop"
       break
     end
-    students << {name: name} # << means .push/.append
+    students << {name: name.capitalize} # << means .push/.append
     puts "Enter their cohort"
     cohort = gets.chomp
-    students[i][:cohort] = cohort
+    until !cohort.empty?
+      puts "Please enter something"
+      cohort = gets.chomp
+    end
+    students[i][:cohort] = cohort.capitalize # standardising the input
     puts "Enter the country they are from"
     country = gets.chomp
-    students[i][:country] = country
+    until !country.empty?
+      puts "Please enter something"
+      country = gets.chomp
+    end
+    students[i][:country] = country.capitalize
     puts "Enter their height in cm"
     height = gets.chomp
-    students[i][:height] = height
+    until height.match(/^\d+$/) # making sure they only enter digits
+      puts "Please enter a number"
+      height = gets.chomp
+    end
+    students[i][:height] = "#{height} cm"
     puts "Now we have #{students.count} students"
     i += 1
   end
@@ -34,23 +45,32 @@ end
 
 # printing each student
 def print(students)
-  puts "Print student names beginning with which letter? Type enter to print all students"
-  letter = gets.chomp.upcase
   index = 0
-  until index == students.length do
-    if (students[index][:name].upcase[0] == letter || letter.empty?) && students[index][:name].length < 12
-      puts "#{index + 1}."
-      puts "#{students[index][:name]} (#{students[index][:cohort]} cohort)".center(30)
-      puts "Country: #{students[index][:country]}".center(30)
-      puts "Height: #{students[index][:height]}cm".center(30)
+  if students == nil
+    return nil
+  else
+    puts "Enter the cohort you wish to print"
+    cohort = gets.chomp.capitalize
+    selected_cohort = students.select{|student| student if student[:cohort] == cohort}
+    puts "Print student names beginning with which letter? Type enter to print all students"
+    letter = gets.chomp.upcase
+    until index == selected_cohort.length do
+      if (selected_cohort[index][:name].upcase[0] == letter || letter.empty?) && selected_cohort[index][:name].length < 12
+        puts "#{index + 1}."
+        puts "#{selected_cohort[index][:name]} (#{selected_cohort[index][:cohort]} cohort)".center(30)
+        puts "Country: #{selected_cohort[index][:country]}".center(30)
+        puts "Height: #{selected_cohort[index][:height]}".center(30)
+      end
+      index += 1
     end
-    index += 1
   end
 end
 
 # printing the total number of students
 def print_footer(students)
-  if students.length == 1
+  if students == nil
+    puts "No students to print"
+  elsif students.length == 1
     puts "Overall, we have 1 great student"
   else
     puts "Overall, we have #{students.count} great students"
